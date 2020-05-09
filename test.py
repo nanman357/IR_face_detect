0f81cfb7dd1b6954f5a1a9673602b11e2ff4b353
Dave R <nanman357@gmail.com>
2020-05-09 17:00:06 +0200

    haar cascades

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
-print("test")
+import face_recognition
+import os
+import cv2
+import numpy as np
+face_cascade = cv2.CascadeClassifier("data\\haarcascades\\haarcascade_frontalface_default.xml")
+video = cv2.VideoCapture("C:\\Users\\Dave\\Downloads\\ytdl\\1.mp4")
+a=1
+
+while True:
+    a=a+1
+    check, frame = video.read()
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(grayscale, 1.3, 5)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y), (x+w,y+h), (255,0,0),1)
+        roi_gray = grayscale[y:y+h, x:x+w]
+        #roi_color = img[y:y+h, x:x+w]
+
+    cv2.imshow('Capturing', grayscale)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+print(a)
+video.release()
+cv2.destroyAllWindows()
