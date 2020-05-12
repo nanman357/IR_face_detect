0043b1aa7a005a0f4afe84207560573bf225e9b6
Dave R <nanman357@gmail.com>
2020-05-12 22:58:52 +0200

    detecting max heat-point on face

--- IR_face_detect_image.py
+++ IR_face_detect_image.py
@@ -1,26 +1,31 @@
 import cv2
-from os import listdir
-from os.path import isfile, join
 import glob
 
-face_cascade = cv2.CascadeClassifier('data\\haarcascades\\haarcascade_frontalface_default.xml')
+path_img = 'C:\\Users\\Dave\\Downloads\\tufts-face-database-thermal-td-ir\\'
+file_imgs = [f for f in glob.glob(path_img + 'TD_IR_E**\\' + '**/*.jpg', recursive=True)]
+path_cascades = 'data\\haarcascades\\'
+files_cascades = [c for c in glob.glob(path_cascades + '**')]
 
-path = 'C:\\Users\\Dave\\Downloads\\tufts-face-database-thermal-td-ir\\'
-files = [f for f in glob.glob(path + "**/*.jpg", recursive=True)]
-face_count = 0
+# for c in files_cascades:
+face_cascade = cv2.CascadeClassifier('data\haarcascades\haarcascade_frontalface_default.xml')
+positive_face_id = 0
 
-for f in files:
+for f in file_imgs:
     img = cv2.imread(f)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
     for (x, y, w, h) in faces:
-        face_count = face_count + 1
+        positive_face_id = positive_face_id + 1
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    # cv2.imshow('img', img)
+    cv2.imshow('img', img)
     cv2.waitKey()
-    # print(face_count)
 
-print('sensitivity: ' + str(face_count / len(files)))
+sensitivity = (positive_face_id / len(file_imgs))
+print(c + ': ' + str(sensitivity))
 
-# TODO:
-# add loop for checking cascade classifiers, and images from the front
+# RESULT
+# data\haarcascades\haarcascade_frontalface_alt.xml: 0.5813953488372093
+# data\haarcascades\haarcascade_frontalface_alt2.xml: 0.6386404293381037
+# data\haarcascades\haarcascade_frontalface_alt_tree.xml: 0.0017889087656529517
+# data\haarcascades\haarcascade_frontalface_default.xml: 0.8425760286225402
+
---
+++ temp_detect.py
@@ -0,0 +1,28 @@
+import cv2
+import glob
+import numpy as np
+
+path_img = 'C:\\Users\\Dave\\Downloads\\tufts-face-database-thermal-td-ir\\'
+file_imgs = [f for f in glob.glob(path_img + 'TD_IR_E**\\' + '**/*.jpg', recursive=True)]
+path_cascades = 'data\\haarcascades\\haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(path_cascades)
+positive_face_id = 0
+
+for f in file_imgs:
+    img = cv2.imread(f)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    height, width = gray.shape
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        positive_face_id = positive_face_id + 1
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi = gray[y:y+h, x:x+w]
+        (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(roi)
+        print(height, width, h, w, maxLoc, maxVal)
+        cv2.circle(img, (maxLoc[0]+x, maxLoc[1]+y), 5, (0, 0, 255), 2)
+        #cv2.circle(roi, (maxLoc[0], maxLoc[1]), 5, (0, 0, 255), 2)
+        cv2.imshow('img', img)
+        cv2.waitKey()
+
+sensitivity = (positive_face_id / len(file_imgs))
+print(c + ': ' + str(sensitivity))
