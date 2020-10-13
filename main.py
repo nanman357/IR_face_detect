d30b421a672254775af7dfc145129d0ba1a33884
Dave R <nanman357@gmail.com>
2020-10-13 22:01:45 +0200

    added black-body roi comparion to face max temp

--- main.py
+++ main.py
@@ -55,10 +55,14 @@ thermal_video = WebcamVideoStream(src=thermal_url).start()
 #print(width_color, height_color)
 #print(width_thermal, heigth_thermal)
 
-bb_roi = None
+color_width_original = 1920
+color_height_origial = 1080
+color_width_crop = 320
+color_height_crop = 60
+
+roi_bb = None
 while True:
     frame_color = color_video.read()
-
     frame_thermal = thermal_video.read()
 
     #print('thermal', frame_thermal.shape)
@@ -71,25 +75,38 @@ while True:
 
 
     # crop color frame to size roughly of thermal frame
-    frame_color = frame_color[60:1020,320:1600]
+    frame_color = frame_color[color_height_crop:color_height_origial - color_height_crop,
+                              color_width_crop:color_width_original - color_width_crop]
 
     #select area of black-body on thermal camera
-    while bb_roi == None:
-        bb_roi = cv2.selectROI(frame_thermal)
+    while roi_bb == None:
+        roi_bb = cv2.selectROI(frame_thermal)
         cv2.destroyAllWindows()
-
+    (minVal_bb, maxVal_bb, minLoc_bb, maxLoc_bb) = cv2.minMaxLoc(roi_bb)
     for(x,y,w,h) in faces:
-        cv2.rectangle(frame_thermal,
-                      (x,y),
-                      (x+w,y+h),
-                      (255,0,0),1)
         #roi_gray = grayscale_color[y:y+h, x:x+w]
         #roi_color = frame_color[y:y+h, x:x+w]
-        roi_thermal = grayscale_thermal[y:y+h, x:x+w]
-        (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(roi_thermal)
+
+        roi_thermal = grayscale_thermal[y - color_height_crop : y + h - color_height_crop,
+                                        x - color_width_crop : x + w - color_width_crop]
+        (minVal_thermal, maxVal_thermal, minLoc_thermal, maxLoc_thermal) = cv2.minMaxLoc(roi_thermal)
         #print(height, width, h, w, maxLoc, maxVal)
         #face_frame_count = face_frame_count + 1
-        cv2.circle(frame_thermal, (maxLoc[0] + x, maxLoc[1] + y), 5, (0, 0, 255), 2)
+        print('bb: ', maxVal_bb)
+        print('thermal: ', maxVal_thermal)
+        if maxVal_bb >= maxVal_thermal:
+            test = (255,0,0)
+        else:
+            test = (0,0,255)
+
+        cv2.rectangle(frame_thermal,
+                      (x-color_width_crop,y-color_height_crop),
+                      (x+w-color_width_crop,y+h-color_height_crop),
+                      test,1)
+        cv2.circle(frame_thermal,
+                   (maxLoc_thermal[0] + x - color_width_crop,
+                    maxLoc_thermal[1] + y - color_height_crop),
+                   5, (0, 0, 255), 1)
 
     frame_thermal = imutils.resize(frame_thermal, width=640)
     frame_color = imutils.resize(frame_color, width=640)
